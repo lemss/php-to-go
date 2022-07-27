bdfd114f3d4747bee47c2975e024419ec13be5ce
lemss <lemss@126.com>
2022-07-27 13:44:03 +0800

    feat(misc): add uniqid function

--- misc.go
+++ misc.go
@@ -2,3 +2,9 @@
 func Echo(args ...interface{}) {
 	fmt.Print(args...)
 }
+
+// Uniqid 实现类似PHP的uniqid函数
+func Uniqid(prefix string) string {
+	now := time.Now()
+	return fmt.Sprintf("%s%08x%05x", prefix, now.Unix(), now.UnixNano()%0x100000)
+}
