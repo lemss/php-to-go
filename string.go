70a4ac9d5af97acb9df2ae92d38b232eaa8fc226
lemss <lemss@126.com>
2022-07-29 11:10:03 +0800

    feat(string): add Unserialize() function

--- string.go
+++ string.go
@@ -2,3 +2,8 @@
 func Serialize(value interface{}) ([]byte, error) {
 	return serialize.Marshal(value)
 }
+
+// Unserialize 实现类似PHP的unserialize()函数
+func Unserialize(data []byte) (interface{}, error) {
+	return serialize.UnMarshal(data)
+}
