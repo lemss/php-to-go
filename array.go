ac82204e3a7539f4b00f9f5271c138aa81e499d6
lemss <lemss@126.com>
2022-08-01 11:16:59 +0800

    feat(array): add IsArray() function

--- array.go
+++ array.go
@@ -1,1 +1,14 @@
+// IsArray 实现类似PHP的is_array()函数
+func IsArray(v interface{}) bool {
+	t := reflect.TypeOf(v)
+	switch t.Kind() {
+	// case reflect.Map:
+	// 	return true
+	case reflect.Slice:
+		return true
+	default:
 
+	}
+
+	return false
+}
