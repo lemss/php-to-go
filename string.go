fb828667263ac045a40a38c1cc73660d5c7eae13
lemss <lemss@126.com>
2022-08-29 13:11:01 +0800

    feat(string): add Stripos() function

--- string.go
+++ string.go
@@ -24,3 +24,21 @@ func Strpos(haystack, needle string, offset int) int {
 	}
 	return pos + offset
 }
+
+// Stripos 实现类似PHP的stripos()函数
+func Stripos(haystack, needle string, offset int) int {
+	length := len(haystack)
+	if length == 0 || offset > length || -offset > length {
+		return -1
+	}
+
+	haystack = haystack[offset:]
+	if offset < 0 {
+		offset += length
+	}
+	pos := strings.Index(strings.ToLower(haystack), strings.ToLower(needle))
+	if pos == -1 {
+		return -1
+	}
+	return pos + offset
+}
