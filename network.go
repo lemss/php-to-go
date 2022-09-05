bad14f55655271328e14e76dfdad8df71d29efb0
lemss <lemss@126.com>
2022-09-05 10:44:13 +0800

    feat(network): add Gethostbynamel() function

--- network.go
+++ network.go
@@ -17,3 +17,19 @@ func Gethostbyname(hostname string) (string, error) {
 	}
 	return "", err
 }
+
+// Gethostbynamel 实现类似PHP的gethostbynamel()函数
+// 获取与给定 Internet 主机名对应的 IPv4 地址
+func Gethostbynamel(hostname string) ([]string, error) {
+	ips, err := net.LookupIP(hostname)
+	if ips != nil {
+		var ipstrs []string
+		for _, v := range ips {
+			if v.To4() != nil {
+				ipstrs = append(ipstrs, v.String())
+			}
+		}
+		return ipstrs, nil
+	}
+	return nil, err
+}
