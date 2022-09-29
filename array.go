a65775586d6b547b0d4d670d80189ce62df13177
lemss <lemss@126.com>
2022-09-29 13:53:33 +0800

    feat(array): add ArrayFill() function.

--- array.go
+++ array.go
@@ -21,3 +21,14 @@ func ArrayKeys(array map[string]interface{}) []string {
 	}
 	return keys
 }
+
+// ArrayFill 实现类似PHP的array_fill()函数
+func ArrayFill(startIndex int, num uint, value interface{}) map[int]interface{} {
+	m := make(map[int]interface{})
+	var i uint
+	for i = 0; i < num; i++ {
+		m[startIndex] = value
+		startIndex++
+	}
+	return m
+}
