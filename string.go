a6336d3ab3565144c1a301560f57a2e329a3a95b
lemss <lemss@126.com>
2022-10-21 10:26:05 +0800

    feat(string): add Strrpos() function

--- string.go
+++ string.go
@@ -42,3 +42,22 @@ func Stripos(haystack, needle string, offset int) int {
 	}
 	return pos + offset
 }
+
+// Strrpos 实现类似PHP的strrpos()函数
+func Strrpos(haystack, needle string, offset int) int {
+	pos, length := 0, len(haystack)
+	if length == 0 || offset > length || -offset > length {
+		return -1
+	}
+
+	if offset < 0 {
+		haystack = haystack[:offset+length+1]
+	} else {
+		haystack = haystack[offset:]
+	}
+	pos = strings.LastIndex(haystack, needle)
+	if offset > 0 && pos != -1 {
+		pos += offset
+	}
+	return pos
+}
